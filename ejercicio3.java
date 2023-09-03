0f33489d7f1b534718bc8c8ddc7c6f63e3dbb8be
mirko222 <143762066+mirko222@users.noreply.github.com>
2023-09-02 19:30:22 -0500

    Create ejercicio3.java

---
+++ ejercicio3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    int numA, numB;
+	    
+	    Scanner leer = new Scanner(System.in);
+	    
+	    
+		System.out.println("ingrese un numero:");
+		numA = leer.nextInt();
+		numB = leer.nextInt();
+		if(numA>0 && numB>0){
+		  System.out.println("true");
+		 
+		}
+ 
+	}
+}
