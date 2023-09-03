3101a0b876691b01d093c46ab38156a96c8c907c
mirko222 <143762066+mirko222@users.noreply.github.com>
2023-09-02 19:11:21 -0500

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    int numero;
+	    
+	    Scanner leer = new Scanner(System.in);
+	    
+	    
+		System.out.println("ingrese un numero:");
+		nuemro = leer.nextint()
+		
+		if(numero>0 && numero%2 != 0){
+		  System.out.println("true")
+		 
+		}
+ 
+	}
+}
